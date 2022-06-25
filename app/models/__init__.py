828f0a2f401336b513d5ffd4ac1288e6c6f355bb
fxuls <32501694+fxuls@users.noreply.github.com>
2022-06-25 16:16:41 -0400

    Add Restaurant

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,4 @@ from .location import Location
 from .cuisine_type import CuisineType
 from .timeslot import Timeslot
 from .occasion import Occasion
+from .restaurant import Restaurant
---
+++ app/models/restaurant.py
@@ -0,0 +1,28 @@
+from .db import db
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), nullable=False)
+    url = db.Column(db.String(120), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float(precision=2))
+    capacity = db.Column(db.Integer)
+    address_line_1 = db.Column(db.String(85), nullable=False)
+    address_line_2 = db.Column(db.String(85), nullable=False)
+    zip_code = db.Column(db.Integer, nullable=False)
+    reservation_notes = db.Column(db.String(255))
+    preview_image_url = db.Column(db.String(120))
+
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    cuisine_id = db.Column(db.Integer, db.ForeignKey("cuisine_types.id"))
+    opening_time_id = db.Column(db.Integer, db.ForeignKey("timeslots.id"), nullable=False)
+    closing_time_id = db.Column(db.Integer, db.ForeignKey("timeslots.id"), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"), nullable=False)
+
+    owner = db.relationship("User")
+    cuisine_type = db.relationship("CuisineType")
+    opening_time = db.relationship("Timeslot", foreign_keys=[opening_time_id])
+    closing_time = db.relationship("Timeslot", foreign_keys=[closing_time_id])
+    locaiton = db.relationship("Location")
