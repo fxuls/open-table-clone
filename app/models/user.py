aa6c8f5dfb767a8585e6fc5b843a07b144692871
fxuls <32501694+fxuls@users.noreply.github.com>
2022-06-25 13:38:42 -0400

    Update columns

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,14 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
+
+    location = db.relationship("Location", back_populates="users")
+
 
     @property
     def password(self):
