a11edba0a28afc83a9b4397fd0b26d6b52c1d6f0
fxuls <32501694+fxuls@users.noreply.github.com>
2022-06-25 14:13:57 -0400

    Add CuisineType model

---
+++ app/models/cuisine_type.py
@@ -0,0 +1,9 @@
+from .db import db
+
+class CuisineType(db.Model):
+    __tablename__ = 'cuisine_types'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False, unique=True)
+
+    restaurants = db.relationship("Restaurant", back_populates="cuisine_type")
