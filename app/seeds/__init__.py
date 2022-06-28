e06cad8e7d7a7def6a400e84443440e57c86d883
gisales92 <isales.greg@gmail.com>
2022-06-27 21:48:59 -0700

    added timeslots seed and updated timeslot model/migration

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .locations import seed_locations, undo_locations
 from .cuisine_types import seed_cuisine_types, undo_cuisine_types
+from .timeslots import seed_timeslots, undo_timeslots
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -14,12 +15,14 @@ def seed():
     seed_locations()
     seed_users()
     seed_cuisine_types()
+    seed_timeslots()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_timeslots()
     undo_cuisine_types()
     undo_users()
     undo_locations()
