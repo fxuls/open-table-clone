ffd9c1e6dc8434ac5f472fbc0f8cba9dd1e24e7e
gisales92 <isales.greg@gmail.com>
2022-06-29 21:55:24 -0700

    Added favorites model/migration

---
+++ app/models/favorite.py
@@ -0,0 +1,17 @@
+from .db import db
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
+
+    user = db.relationship("User")
+    restaurant = db.relationship("Restaurant")
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user': self.user.to_dict(),
+            'restaurant': self.restaurant.to_dict(),
+        }
