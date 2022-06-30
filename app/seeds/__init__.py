95f0b6dcc4641b3645dd3388ed62992c4ed23bcc
gisales92 <isales.greg@gmail.com>
2022-06-29 20:45:59 -0700

    Added seed reservations for demo user

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -7,6 +7,7 @@ from .occasions import seed_occasions, undo_occasions
 from .restaurants import seed_restaurants, undo_restaurants
 from .images import seed_images, undo_images
 from .reviews import seed_reviews, undo_reviews
+from .reservations import seed_reservations, undo_reservations
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -24,12 +25,14 @@ def seed():
     seed_restaurants()
     seed_images()
     seed_reviews()
+    seed_reservations()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_reservations()
     undo_reviews
     undo_images()
     undo_restaurants()
---
+++ app/seeds/reservations.py
@@ -0,0 +1,43 @@
+from app.models import db, Reservation
+from datetime import date
+
+# Adds restaurants of each cuisine type and location to seed the database
+def seed_reservations():
+    amer = Reservation(
+        party_size=5, day=date(year=2022, month=6, day=20),
+        user_id=1, restaurant_id=1, timeslot_id=60)
+    ital = Reservation(
+        party_size=4, day=date(year=2022, month=4, day=20), special_request="Outside seating",
+        user_id=1, restaurant_id=6, timeslot_id=66, occasion_id=5)
+    stea = Reservation(
+        party_size=2, day=date(year=2022, month=7, day=10),
+        user_id=1, restaurant_id=11, timeslot_id=77, occasion_id=3)
+    seaf = Reservation(
+        party_size=4, day=date(year=2022, month=8, day=31), special_request="Outside seating",
+        user_id=1, restaurant_id=16, timeslot_id=75, occasion_id=1)
+    fren = Reservation(
+        party_size=5, day=date(year=2022, month=9, day=13),
+        user_id=1, restaurant_id=21, timeslot_id=71)
+    indi = Reservation(
+        party_size=3, day=date(year=2022, month=10, day=8),
+        user_id=1, restaurant_id=26, timeslot_id=77, occasion_id=2)
+    mexi = Reservation(
+       party_size=8, day=date(year=2022, month=11, day=26), special_request="Outside seating",
+        user_id=1, restaurant_id=3, timeslot_id=75, occasion_id=2)
+    japa = Reservation(
+        party_size=2, day=date(year=2022, month=12, day=30),
+        user_id=1, restaurant_id=8, timeslot_id=81, occasion_id=1)
+
+
+    db.session.add_all([amer, ital, stea, seaf, fren, indi, mexi, japa])
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE the reservations table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_reservations():
+    db.session.execute('TRUNCATE reservations RESTART IDENTITY CASCADE;')
+    db.session.commit()
