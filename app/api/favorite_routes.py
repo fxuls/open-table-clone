4918bac909b9eae3185ced66bfafa432fb8cb21a
gisales92 <isales.greg@gmail.com>
2022-06-30 22:27:26 -0700

    Finished GET /my/favorites route

--- app/api/favorite_routes.py
+++ app/api/favorite_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import current_user, login_required
+import json
 
 from app import models
 from ..models import Favorite, Restaurant
@@ -13,20 +14,27 @@ def get_user_favorites():
     Gets a list of all user's favorited restaurants
     """
 
-    favs = Favorite.query.filter(Favorite.user_id == current_user.to_dict().id)
+    query = Favorite.query.filter(Favorite.user_id == current_user.id)
 
-    # restaurants = [rest.to_dict() for rest in query.all()]
-    return jsonify({ "restaurants": favs }), 200
+    faves = [fav.to_dict() for fav in query.all()]
+    filtered_faves = []
+    extra_keys = ['opening_time', 'closing_time', 'images', 'address_line_1', 'address_line_2', 'capacity', 'owner_id', 'reservation_notes', 'zip_code']
+    for fave in faves:
+        del fave['user']
+        del fave['id']
+        for unwanted_key in extra_keys:
+            del fave['restaurant'][unwanted_key]
+        filtered_faves.append(fave['restaurant'])
+    return jsonify({ "restaurants": filtered_faves }), 200
 
 
-@favorite_routes.route('/<path:restaurant_url>')
-def get_restaurant_details(restaurant_url):
-    """
-    Get details of a restaurant by its url
-    """
-    restaurant = Restaurant.query.filter(Restaurant.url == restaurant_url).first()
+@favorite_routes.route('/', methods=['POST'])
+@login_required
+def add_favorite():
+    pass
 
-    if restaurant is None:
-        return jsonify({ "message": "Restaurant couldn't be found", "status_code": 404}), 404
 
-    return restaurant.to_dict(), 200
+@favorite_routes.route('/:restaurant_id', methods=['DELETE'])
+@login_required
+def delete_favorite(restaurant_id):
+    pass
