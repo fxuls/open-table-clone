9b742951b80815c4aaa915c265812376dae78a50
gisales92 <isales.greg@gmail.com>
2022-06-30 21:04:45 -0700

    Added blueprint to init for testing

--- app/api/favorite_routes.py
+++ app/api/favorite_routes.py
@@ -1,32 +1,25 @@
 from flask import Blueprint, jsonify, request
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from app import models
-from ..models import Favorite
+from ..models import Favorite, Restaurant
 
-restaurant_routes = Blueprint('restaurants', __name__)
+favorite_routes = Blueprint('favorites', __name__)
 
-@restaurant_routes.route('/', methods=['GET'])
-def get_retaurants():
+@favorite_routes.route('/', methods=['GET'])
+@login_required
+def get_user_favorites():
     """
-    Gets a list of all restaurants
-
-    Results are filtered according to location_id and cuisine_id
-    passed in through body
+    Gets a list of all user's favorited restaurants
     """
-    body = request.get_json()
 
-    query = Restaurant.query
-    if 'cuisine_id' in body:
-        query = query.filter(Restaurant.cuisine_id == body['cuisine_id'])
-    if 'location_id' in body:
-        query = query.filter(Restaurant.location_id == body['location_id'])
+    favs = Favorite.query.filter(Favorite.user_id == current_user.to_dict().id)
 
-    restaurants = [rest.to_dict() for rest in query.all()]
-    return jsonify({ "restaurants": restaurants }), 200
+    # restaurants = [rest.to_dict() for rest in query.all()]
+    return jsonify({ "restaurants": favs }), 200
 
 
-@restaurant_routes.route('/<path:restaurant_url>')
+@favorite_routes.route('/<path:restaurant_url>')
 def get_restaurant_details(restaurant_url):
     """
     Get details of a restaurant by its url
