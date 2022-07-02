369060775ce9442a9f085ada1b48aacd89552236
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-02 14:48:58 -0400

    Add validation for occasion_id

--- app/forms/reservation_form.py
+++ app/forms/reservation_form.py
@@ -3,7 +3,7 @@ import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TimeField, DateField
 from wtforms.validators import DataRequired, ValidationError, NumberRange, Length
-from app.models import Restaurant, Timeslot
+from app.models import Restaurant, Timeslot, Occasion
 
 def restaurant_exists(form, field):
     """
@@ -29,10 +29,18 @@ def validate_day_is_not_in_past(form, field):
     if field.data < datetime.date.today():
         raise ValidationError("Day cannot be in the past")
 
+def validate_occasion(form, field):
+    """
+    Validate that occasion_id points to a valid occasions entry
+    """
+    occasion = Occasion.query.get(field.data)
+    if occasion is None:
+        raise ValidationError("occasion_id must point to a valid occasion")
+
 class ReservationForm(FlaskForm):
     restaurant_id = IntegerField('restaurant_id', validators=[DataRequired(), restaurant_exists])
     party_size = IntegerField('party_size', validators=[DataRequired(), NumberRange(min=1, message="party_size must be 1 or greater")])
     timeslot = TimeField('timeslot', format='%H:%M:%S', validators=[DataRequired(message="timeslot is required in format hh:mm:ss"), validate_timeslot])
     day = DateField('day', format='%Y-%m-%d', validators=[DataRequired(message="day is required in format yyyy-mm-dd"), validate_day_is_not_in_past])
     special_request = StringField('special_request', validators=[Length(max=600, message="special_request must be 600 characters or fewer")])
-    occasion_id = IntegerField('occasion_id')
+    occasion_id = IntegerField('occasion_id', validators=[validate_occasion])
