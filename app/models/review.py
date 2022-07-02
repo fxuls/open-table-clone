d5a857f9b7e9e63fc1af87aa335a24c9adfa21cf
gisales92 <isales.greg@gmail.com>
2022-07-02 12:03:14 -0700

    Added back_populates for reviews

--- app/models/review.py
+++ app/models/review.py
@@ -14,7 +14,7 @@ class Review(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
 
-    user = db.relationship("User")
+    user = db.relationship("User", back_populates="reviews")
     restaurant = db.relationship("Restaurant", back_populates="reviews")
 
     def to_dict(self):
