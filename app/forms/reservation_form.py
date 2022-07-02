26b414dd45eaefa86fac2af3b479adfe02769a0a
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-02 13:54:21 -0400

    Inital commit

---
+++ app/forms/reservation_form.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TimeField, DateField
+from wtforms.validators import DataRequired, ValidationError, NumberRange
+from app.models import Restaurant
+
+def restaurant_exists(form, field):
+    # check if restaurant exists
+    restaurant_id = field.data
+    restaurant = Restaurant.query.get(restaurant_id)
+    if restaurant is None:
+        raise ValidationError("Restaurant couldn't be found")
+
+class ReservationForm(FlaskForm):
+    restaurant_id = IntegerField('restaurant_id', validators=[DataRequired()])
+    party_size = IntegerField('party_size', validators=[DataRequired(), NumberRange(min=1, message="Party size must be 1 or greater")])
+    timeslot = TimeField('timeslot', validators=[DataRequired()])
+    day = DateField('day', validators=[DataRequired()])
+    special_request = StringField('special_request')
+    occasion_id = IntegerField('occasion_id')
