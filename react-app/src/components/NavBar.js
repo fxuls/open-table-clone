4d4a482dc95c72f9d6365f453a5d8b24d0ae04ea
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-12 19:53:31 -0400

    Add link to masthead

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,20 +1,22 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 
 const NavBar = () => {
   return (
-    <nav className='nav-header'>
-      <div className='flex-row'>
-        <img className='masthead' src="//cdn.otstatic.com/cfe/9/images/opentable-logo-153e80.svg" />
+    <nav className="nav-header">
+      <div className="flex-row">
+        <a href="/">
+          <img
+            className="masthead"
+            src="//cdn.otstatic.com/cfe/9/images/opentable-logo-153e80.svg"
+          />
+        </a>
       </div>
 
-      <div className='flex-row'>
-        Right
-      </div>
+      <div className="flex-row">Right</div>
     </nav>
   );
-}
+};
 
 export default NavBar;
