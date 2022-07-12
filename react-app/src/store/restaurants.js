37b287cd659869b64b00e61162b3095a8137485f
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-11 20:18:31 -0400

    Add aciton creators and comments

--- react-app/src/store/restaurants.js
+++ react-app/src/store/restaurants.js
@@ -2,5 +2,24 @@ export const SET_RESTAURANT = 'restaurants/SET_RESTAURANT';
 export const SET_RESTAURANTS = 'restaurants/SET_RESTAURANTS';
 export const REMOVE_RESTAURANT = 'restaurants/REMOVE_RESTAURANT';
 
+// selectors
 export const restaurantSelector = (restaurantId) => (state) => state.restaurants[restaurantId];
 export const allRestaurantsSelector = (state) => state.restaurants;
+
+// SET_RESTAURANTS action creator
+export function setResaurants(restaurants) {
+    return {
+        type: SET_RESTAURANTS,
+        restaurants,
+    };
+}
+
+// SET_RESTAURANT action creator
+export function setRestaurant(restaurant) {
+    return {
+        type: SET_RESTAURANT,
+        restaurant,
+    }
+}
+
+
