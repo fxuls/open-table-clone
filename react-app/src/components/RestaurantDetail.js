c4ed4e89fa83dd8144b3d72c4babca9167223d52
gisales92 <isales.greg@gmail.com>
2022-07-13 14:55:24 -0700

    Added locatoin info

--- react-app/src/components/RestaurantDetail.js
+++ react-app/src/components/RestaurantDetail.js
@@ -17,6 +17,7 @@ function RestaurantDetail() {
   }, [dispatch, updated, url]);
 
   const restaurant = useSelector(restaurantActions.restaurantSelector)
+
   const fixTimes = (timeString) => {
     const stringArr = timeString?.split(":");
     const intArr = [];
@@ -26,11 +27,12 @@ function RestaurantDetail() {
     let amPm = "AM";
     if (intArr[0] > 12) {
         intArr[0] -= 12;
-        amPm = "PM"
+        amPm = "PM";
     }
   return `${intArr[0]}:${stringArr[1]} ${amPm}`
-  }
-    return (
+  };
+
+  return (
       <div className="restDetail">
         <img src={`${restaurant?.preview_image_url}`} alt="Restaurant Header" className="restHeaderImg"/>
         <div className="restProfile">
@@ -41,13 +43,19 @@ function RestaurantDetail() {
                 : null}
             <p className="restInfo">{restaurant?.cuisine_type} • {"$".repeat(restaurant?.price)} • {restaurant?.location.city}</p>
             </div>
+            {restaurant ?
             <div className="restDetails">
                 <h4>Additional Info</h4>
                 <h6 className="rest-detail-title">Opening Time</h6>
-                <p className="rest-detail-info">{fixTimes(restaurant?.opening_time)}</p>
+                <p className="rest-detail-info">{fixTimes(restaurant.opening_time)}</p>
                 <h6 className="rest-detail-title">Closing Time</h6>
-                <p className="rest-detail-info">{fixTimes(restaurant?.closing_time)}</p>
+                <p className="rest-detail-info">{fixTimes(restaurant.closing_time)}</p>
+                <h6 className="rest-detail-title">Location</h6>
+                <p className="rest-detail-info">{restaurant.address_line_1}</p>
+                <p className="rest-detail-info">{restaurant.address_line_2}</p>
+                <p className="rest-detail-info">{`${restaurant.location?.city}, ${restaurant.location?.state}`}</p>
             </div>
+            : null }
             <div className="restGallery"></div>
         </div>
       </div>
