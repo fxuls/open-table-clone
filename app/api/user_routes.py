618a2676300eea6a3da29c9c83d5756998b0e3f9
gisales92 <isales.greg@gmail.com>
2022-07-12 20:30:07 -0700

    Added error message to user backend route to match documentation

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from app.models import User
+
 
 user_routes = Blueprint('users', __name__)
 
@@ -10,4 +11,10 @@ def user(user_id):
     Gets details of a user by user_id
     """
     user = User.query.get(user_id)
+    # Error message for no user with that id
+    if user is None:
+        return jsonify({
+            "message": "User couldn't be found",
+            "status_code": 404,
+        }), 404
     return user.to_dict()
