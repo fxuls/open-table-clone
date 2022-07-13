43c748a0fa462f9d391c33ca5e644fe070c1c8fe
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-13 18:42:14 -0400

    Render nav links conditionally

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCompass } from "@fortawesome/free-regular-svg-icons";
+
 import { userSelector } from "../../store/session";
+import UnauthenticatedNav from "./UnauthenticatedNav";
+import AuthenticatedNav from "./AuthenticatedNav";
 
 const NavBar = () => {
 
   const sessionUser = useSelector(userSelector);
-  
+
   return (
     <nav className="nav-header">
       <div className="flex-row">
@@ -26,19 +28,7 @@ const NavBar = () => {
         </div>
       </div>
 
-      <div className="flex-row nav-links">
-        <Link to="/login">
-          <button className="nav-button hover-effect login-button">
-            Log in
-          </button>
-        </Link>
-
-        <Link to="/sign-up">
-          <button className="nav-button hover-effect sign-up-button">
-            Sign up
-          </button>
-        </Link>
-      </div>
+      {sessionUser ? <AuthenticatedNav /> : <UnauthenticatedNav />}
     </nav>
   );
 };
