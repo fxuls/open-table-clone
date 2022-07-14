39d86cbeefb4d64b2125379cbfcf612d022a6bea
gisales92 <isales.greg@gmail.com>
2022-07-13 20:56:38 -0700

    edited app.before_request to handle error

--- app/__init__.py
+++ app/__init__.py
@@ -59,10 +59,9 @@ CORS(app)
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':
-        if request.headers.get('X-Forwarded-Proto') == 'http':
-            url = request.url.replace('http://', 'https://', 1)
-            code = 301
-            return redirect(url, code=code)
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 
 @app.after_request
