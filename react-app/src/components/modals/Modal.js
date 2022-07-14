3ba7218f62f99dce2c3c62082f718b0e637ddc90
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-13 21:16:42 -0400

    Add close button

--- react-app/src/components/modals/Modal.js
+++ react-app/src/components/modals/Modal.js
@@ -1,21 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { modalSelector, hideModal } from "../../store/ui";
 
 const Modal = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const modal = useSelector(modalSelector);
+  const modal = useSelector(modalSelector);
 
-    // close modal when clicked outside it
-    const onOutsideModalClick = (e) => dispatch(hideModal());
-    // stop propagation when click inside modal
-    const onModalClick = (e) => e.stopPropagation();
+  // close modal when clicked outside it
+  const closeModal = (e) => dispatch(hideModal());
+  // stop propagation when click inside modal
+  const onModalClick = (e) => e.stopPropagation();
 
-    return (<div className="modal-overlay" onClick={onOutsideModalClick}>
-        <div className="modal" onClick={onModalClick}>
-            This is the modal contents
+  return (
+    <div className="modal-overlay" onClick={closeModal}>
+      <div className="modal" onClick={onModalClick}>
+        <div className="modal-close" onClick={closeModal}>
+            <FontAwesomeIcon icon={faXmark} size="xl" className="icon" />
         </div>
-    </div>);
-}
+        T
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
