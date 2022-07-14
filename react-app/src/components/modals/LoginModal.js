82aea587b1339cf2b78f78db3903f355c342ccb4
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-13 22:08:22 -0400

    Initial commit

---
+++ react-app/src/components/modals/LoginModal.js
@@ -0,0 +1,12 @@
+export const LOGIN_MODAL = "ui/modals/LOGIN";
+
+const LoginModal = () => {
+  return (
+    <div className="login-modal">
+        <h1>Log in</h1>
+
+    </div>
+  );
+};
+
+export default LoginModal;
