6701c9ece5e850ff87d4ab0e86bfce73fdc630de
gisales92 <isales.greg@gmail.com>
2022-07-14 00:00:54 -0700

    reverting to old before_request

--- app/__init__.py
+++ app/__init__.py
@@ -59,7 +59,7 @@ CORS(app)
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':
-        if request.url.startswith('http://'):
+       if request.headers.get('X-Forwarded-Proto') == 'http':
             url = request.url.replace('http://', 'https://', 1)
             code = 301
             return redirect(url, code=code)
