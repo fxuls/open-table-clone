4c4561e2039e925b95243484c2e23ebe2d06f84e
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-13 20:37:27 -0400

    Add modal

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,58 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/Navigation/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-import { authenticate } from './store/session';
-import { fetchRestaurants } from './store/restaurants';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
+import NavBar from "./components/Navigation/NavBar";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UsersList from "./components/UsersList";
+import User from "./components/User";
+import { authenticate } from "./store/session";
+import { modalSelector } from "./store/ui";
+import { fetchRestaurants } from "./store/restaurants";
+import Modal from "./components/modals/Modal";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
+  const modal = useSelector(modalSelector);
+
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       await dispatch(fetchRestaurants());
 
       setLoaded(true);
     })();
-  }, [dispatch]);
+  }, [dispatch, modal]);
 
   if (!loaded) {
     return null;
   }
 
   return (
-    <BrowserRouter>
-      <NavBar />
+    <div>
+      {modal && <Modal />}
+      <BrowserRouter>
+        <NavBar />
 
-      <Switch>
-        <Route path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </Route>
+        <Switch>
+          <Route path="/" exact={true}>
+            <h1>My Home Page</h1>
+          </Route>
 
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
+          <Route path="/login" exact={true}>
+            <LoginForm />
+          </Route>
 
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
+          <Route path="/sign-up" exact={true}>
+            <SignUpForm />
+          </Route>
 
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
+          <ProtectedRoute path="/users" exact={true}>
+            <UsersList />
+          </ProtectedRoute>
 
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-      </Switch>
-    </BrowserRouter>
+          <ProtectedRoute path="/users/:userId" exact={true}>
+            <User />
+          </ProtectedRoute>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
