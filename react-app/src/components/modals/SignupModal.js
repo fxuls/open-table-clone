6cd7dcb794051d612541ccb3e2a8f1fa9849b0bb
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-14 22:14:25 -0400

    Add base html structure

--- react-app/src/components/modals/SignupModal.js
+++ react-app/src/components/modals/SignupModal.js
@@ -1,10 +1,31 @@
+import { useDispatch } from "react-redux";
+import { showModal } from "../../store/ui";
+import { LOGIN_MODAL } from "./LoginModal";
+
 export const SIGNUP_MODAL = "ui/modals/SIGNUP";
 
 const SignupModal = () => {
+  const dispatch = useDispatch();
+
+  const onSubmit = () => {};
+
   return (
     <div className="signup-modal">
-        <h1>Sign up</h1>
+      <h1 className="form-header">Sign up</h1>
 
+      <form onSubmit={onSubmit}></form>
+
+      <div className="modal-footer">
+        <p>
+          Already have an account?{" "}
+          <a
+            className="text-link"
+            onClick={() => dispatch(showModal(LOGIN_MODAL))}
+          >
+            Log in.
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
