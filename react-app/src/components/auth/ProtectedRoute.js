86f732e617bbc16d4e16ac72541f21cfe6c3164a
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-16 13:36:29 -0400

    Change to use selector

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { userSelector } from '../../store/session';
 import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = props => {
-  const user = useSelector(state => state.session.user)
+  const user = useSelector(userSelector);
+
   return (
     <Route {...props}>
       {(user)? props.children  : <Redirect to='/login' />}
     </Route>
-  )
+  );
 };
 
 
