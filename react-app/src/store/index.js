e399df768abb6ca445ceb44738434dc3dbf6c680
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-16 15:38:53 -0400

    Add favorites reducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import session from './session';
 import restaurants from './restaurants';
 import ui from './ui';
+import favorites from './favorites';
 
 const rootReducer = combineReducers({
   session,
   restaurants,
   ui,
+  favorites,
 });
 
 
