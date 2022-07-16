f09f4edc6450339321728cb01ba6b048743b2203
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-16 14:14:40 -0400

    Add basic html skeleton

--- react-app/src/components/UserProfilePage.js
+++ react-app/src/components/UserProfilePage.js
@@ -1,15 +1,30 @@
 import { useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Link, Redirect, Route, Switch } from "react-router-dom";
 import { userSelector } from "../store/session";
 
-const UserProfilePage = () => {
+const UserProfilePage = (props) => {
   const user = useSelector(userSelector);
 
   // if not logged in redirect to login
   // this should not happen since it is a protected route
   if (!user) return <Redirect to="/login" />;
 
-  return <div>User profile</div>;
+  return <div>
+    <h1>{user.first_name + " " + user.last_name}</h1>
+    <ul>
+        <li><Link to="/profile/favorites">Favorites</Link></li>
+        <li><Link to="/profile/reservations">Reservations</Link></li>
+    </ul>
+    <Switch>
+        <Route path="/profile/favorites">
+            Favorites
+        </Route>
+
+        <Route path="/profile/reservations">
+            Reservations
+        </Route>
+    </Switch>
+  </div>;
 };
 
 export default UserProfilePage;
