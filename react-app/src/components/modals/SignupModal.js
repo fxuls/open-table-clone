9d7517d2144b7413f686654ad5ef3069630a7d10
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-15 21:25:59 -0400

    Add fields and labels

--- react-app/src/components/modals/SignupModal.js
+++ react-app/src/components/modals/SignupModal.js
@@ -1,11 +1,33 @@
-import { useDispatch } from "react-redux";
-import { showModal } from "../../store/ui";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userSelector } from "../../store/session";
+import { showModal, hideModal } from "../../store/ui";
 import { LOGIN_MODAL } from "./LoginModal";
 
 export const SIGNUP_MODAL = "ui/modals/SIGNUP";
 
 const SignupModal = () => {
   const dispatch = useDispatch();
+  const user = useSelector(userSelector);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [errors, setErrors] = useState([]);
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+  // if user is logged in hide modal
+  if (user) {
+    dispatch(hideModal());
+    return null;
+  }
 
   const onSubmit = () => {};
 
@@ -13,7 +35,77 @@ const SignupModal = () => {
     <div className="signup-modal">
       <h1 className="form-header">Sign up</h1>
 
-      <form onSubmit={onSubmit}></form>
+      <form onSubmit={onSubmit}>
+        <div className="form-row">
+          <label htmlFor="firstName">First name</label>
+          <input
+            name="firstName"
+            type="text"
+            placeholder="Jason"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <label htmlFor="firstName" className="field-error">
+            {firstNameError}
+          </label>
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="lastName">Last name</label>
+          <input
+            name="lastName"
+            type="text"
+            placeholder="Smith"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <label htmlFor="lastName" className="field-error">
+            {lastNameError}
+          </label>
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="jason.smith@example.co"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="email" className="field-error">
+            {emailError}
+          </label>
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password" className="field-error">
+            {passwordError}
+          </label>
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="confirmPassword">Confirm password</label>
+          <input
+            name="confirmPassword"
+            type="password"
+            placeholder="password"
+            value={confirmPassword}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="confirmPassword" className="field-error">
+            {confirmPasswordError}
+          </label>
+        </div>
+      </form>
 
       <div className="modal-footer">
         <p>
