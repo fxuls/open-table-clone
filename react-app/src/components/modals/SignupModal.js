bc5224de4cfe36fafed570c110efb72f8427624d
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-15 22:26:17 -0400

    Add front end validations

--- react-app/src/components/modals/SignupModal.js
+++ react-app/src/components/modals/SignupModal.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { userSelector } from "../../store/session";
+import { userSelector, signUp } from "../../store/session";
 import { showModal, hideModal } from "../../store/ui";
 import { LOGIN_MODAL } from "./LoginModal";
 
@@ -22,6 +22,41 @@ const SignupModal = () => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+
+  const validateFirstName = () => {
+    let errorMessage = firstName ? "" : "First name is required";
+    setFirstNameError(errorMessage);
+    return errorMessage === "";
+  };
+
+  const validateLastName = () => {
+    let errorMessage = lastName ? "" : "Last name is required";
+    setLastNameError(errorMessage);
+    return errorMessage === "";
+  };
+
+  const validateEmail = () => {
+    let errorMessage = "";
+    if (!email) errorMessage = "Email is required";
+    else if (!/\S+@\S+\.\S+/.test(email)) errorMessage = "Please enter a valid email";
+    setEmailError(errorMessage);
+    return errorMessage === "";
+  };
+
+  const validatePassword = () => {
+    let errorMessage = password ? "" : "Password is required";
+    setPasswordError(errorMessage);
+    return errorMessage === "";
+  };
+
+  const validateConfirmPassword = () => {
+    let errorMessage = "";
+    if (!confirmPassword) errorMessage = "Please re-enter your password";
+    else if (confirmPassword != password) errorMessage = "Passwords do not match";
+    setConfirmPasswordError(errorMessage);
+    return errorMessage === "";
+  }
 
   // if user is logged in hide modal
   if (user) {
@@ -29,7 +64,19 @@ const SignupModal = () => {
     return null;
   }
 
-  const onSubmit = () => {};
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setHasSubmitted(true);
+
+    // front end validations
+    const validations = [validateFirstName(), validateLastName(), validateEmail(), validatePassword(), validateConfirmPassword()];
+
+    // if there are no errors make request
+    if (!validations.includes(false)) {
+      const data = await dispatch(signUp(firstName, lastName, email, password));
+      if (data) setErrors(data);
+    }
+  };
 
   return (
     <div className="signup-modal">
@@ -99,7 +146,7 @@ const SignupModal = () => {
             type="password"
             placeholder="password"
             value={confirmPassword}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
           <label htmlFor="confirmPassword" className="field-error">
             {confirmPasswordError}
