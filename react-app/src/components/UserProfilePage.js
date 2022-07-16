b45b9c840151fbd76819643c6736176a31bb5be8
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-16 19:07:02 -0400

    Add more wrapping divs and classNames

--- react-app/src/components/UserProfilePage.js
+++ react-app/src/components/UserProfilePage.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect, Route, Switch } from "react-router-dom";
 import { userSelector } from "../store/session";
-import { fetchFavorites, addFavorite, removeFavorite } from "../store/favorites";
+import { fetchFavorites } from "../store/favorites";
 
 const UserProfilePage = (props) => {
   const dispatch = useDispatch();
@@ -17,24 +17,28 @@ const UserProfilePage = (props) => {
   // this should not happen since it is a protected route
   if (!user) return <Redirect to="/login" />;
 
-  return (<div className="profile-page">
-    <div className="header">
-      <h1>{user.first_name + " " + user.last_name}</h1>
-    </div>
-    <ul>
-        <li><Link to="/profile/favorites">Favorites</Link></li>
-        <li><Link to="/profile/reservations">Reservations</Link></li>
-    </ul>
-    <Switch>
-        <Route path="/profile/favorites">
-            Favorites
-        </Route>
+  return (
+    <div className="profile-page">
+      <div className="profile-header">
+        <h1>{user.first_name + " " + user.last_name}</h1>
+      </div>
+      <ul className="profile-navigation">
+        <li>
+          <Link to="/profile/favorites">Favorites</Link>
+        </li>
+        <li>
+          <Link to="/profile/reservations">Reservations</Link>
+        </li>
+      </ul>
+      <div className="profile-content">
+        <Switch>
+          <Route path="/profile/favorites">Favorites</Route>
 
-        <Route path="/profile/reservations">
-            Reservations
-        </Route>
-    </Switch>
-  </div>);
+          <Route path="/profile/reservations">Reservations</Route>
+        </Switch>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfilePage;
