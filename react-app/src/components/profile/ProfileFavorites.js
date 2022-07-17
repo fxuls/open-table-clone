2f9bc3d46201b4133adfead9ee88803c1ee27c49
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-17 19:28:27 -0400

    Add basic skeleton

--- react-app/src/components/profile/ProfileFavorites.js
+++ react-app/src/components/profile/ProfileFavorites.js
@@ -1,5 +1,26 @@
-const ProfileFavorites = () => {
-    return <div>Favorites</div>;
-}
+import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { favoritesSelector } from "../../store/favorites";
+
+const ProfileFavorites = ({ loaded }) => {
+  const favorites = useSelector(favoritesSelector);
+  console.log(favorites);
+
+  return (
+    <div className="profile-content">
+      <h1 className="profile-content-header card-background">Favorites</h1>
+      <div className="favorites card-background">
+        {loaded ? (
+          <div className="restaurant-cards">Favorites will be here</div>
+        ) : (
+          <div className="spinner">
+            <FontAwesomeIcon className="spinner" icon={faCircleNotch} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default ProfileFavorites;
