bc964fd43cd26443d362146dad4aa0e27b7263e9
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-18 03:54:38 -0400

    Update to correct status code

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -101,7 +101,7 @@ def create_review():
         db.session.commit()
 
         return review.to_dict(), 201
-    return {"errors": validation_errors_to_error_messages(form.errors)}, 400
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
 
 
 @reviews_routes.route('/<int:review_id>', methods=['DELETE'])
