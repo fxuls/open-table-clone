590f947830eab5549722c215df75c029400f9ac6
gisales92 <isales.greg@gmail.com>
2022-07-17 21:05:10 -0700

    Added reservations selector to profile reservations

--- react-app/src/components/profile/ProfileReservations.js
+++ react-app/src/components/profile/ProfileReservations.js
@@ -1,7 +1,9 @@
 import * as reservationActions from "../../store/reservations"
+import { useSelector } from "react-redux";
 
 const ProfileReservations = () => {
-    
+const reservations = useSelector(reservationActions.allReservationsSelector());
+
     return <div><h2>Reservations</h2></div>;
 }
 
