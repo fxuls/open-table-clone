3576ef9a71b0e13fd39db57c3f7db875f8403b97
fxuls <32501694+fxuls@users.noreply.github.com>
2022-07-18 14:02:03 -0400

    Add thumbnail image

--- react-app/src/components/profile/ReservationCard.js
+++ react-app/src/components/profile/ReservationCard.js
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 
 const ReservationCard = ({ reservation }) => {
-    const { restaurant } = reservation;
+  const { restaurant } = reservation;
 
-    return (<div className="reservation-card">
-        <Link to={`/restaurants/${restaurant.url}`} className="main-color-hover">
-            {restaurant.name}
-        </Link>
-    </div>);
-}
+  return (
+    <div className="reservation-card">
+      <div className="card-thumbnail">
+        <img
+          src={restaurant.preview_image_url}
+          alt={`Preview image for ${restaurant.name}`}
+        />
+      </div>
+      
+      <Link to={`/restaurants/${restaurant.url}`} className="main-color-hover">
+        {restaurant.name}
+      </Link>
+    </div>
+  );
+};
 
 export default ReservationCard;
